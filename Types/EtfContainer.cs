bb94e2adeab1f9a5705c2a18613fcddc4e51dc43
Adam Chen <encodeous7@gmail.com>
2022-07-18 16:58:08 -0400

    Remove _smallDataStorage

--- Types/EtfContainer.cs
+++ Types/EtfContainer.cs
@@ -7,34 +7,18 @@ namespace EtfDotNet.Types;
 public partial struct EtfContainer : IDisposable
 {
     public static readonly EtfContainer Nil = AsContainer(ArraySegment<byte>.Empty, EtfConstants.NilExt);
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-    private byte[] _smallDataStorage = new byte[4];
+    private bool _returnToPool;
     public ArraySegment<byte> ContainedData;
     public EtfConstants Type;
     internal IEtfComplex? _complexData;
 
-    public EtfContainer()
-    {
-        ContainedData = default;
-        Type = (EtfConstants)0;
-        _complexData = null;
-    }
-
     public static EtfContainer Make(int length, EtfConstants type)
     {
-        if (length <= 4)
-        {
-            var container = new EtfContainer()
-            {
-                Type = type
-            };
-            container.ContainedData = new ArraySegment<byte>(container._smallDataStorage!);
-            return container;
-        }
         return new EtfContainer()
         {
             ContainedData = new ArraySegment<byte>(ArrayPool<byte>.Shared.Rent(length), 0, length),
-            Type = type
+            Type = type,
+            _returnToPool = true
         };
     }
 
@@ -44,9 +28,8 @@ public partial struct EtfContainer : IDisposable
         {
             Type = type,
             ContainedData = data,
+            _returnToPool = false
         };
-        // do not return to array pool
-        BitConverter.TryWriteBytes(container._smallDataStorage, long.MaxValue);
         return container;
     }
 
@@ -55,22 +38,17 @@ public partial struct EtfContainer : IDisposable
         var container = new EtfContainer()
         {
             Type = type,
-            _complexData = complexData
+            _complexData = complexData,
+            _returnToPool = false,
         };
-        // do not return to array pool
-        BitConverter.TryWriteBytes(container._smallDataStorage, long.MaxValue);
         return container;
     }
     
     public void Dispose()
     {
-        if (ContainedData.Count > 4)
-        {
-            if (BitConverter.ToInt64(_smallDataStorage) == long.MaxValue) return;
-            ArrayPool<byte>.Shared.Return(ContainedData.Array);
-        }
-
         _complexData?.Dispose();
+        if (_returnToPool) return;
+        ArrayPool<byte>.Shared.Return(ContainedData.Array);
     }
 
     [Pure]
