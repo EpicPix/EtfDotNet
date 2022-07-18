c1cf2a92855a6841a7dc1da59de6b0f40300d3fc
EpicPix <epicpixminecraft@gmail.com>
2022-07-18 22:45:26 +0200

    Add Utility methods

--- EtfFormat.cs
+++ EtfFormat.cs
@@ -13,11 +13,23 @@ public static class EtfFormat
         }
         return EtfDecoder.DecodeType(input);
     }
+    
+    public static EtfContainer Unpack(byte[] input)
+    {
+        return Unpack(EtfMemory.FromArray(input));
+    }
 
     public static void Pack(EtfContainer etfType, EtfMemory output)
     {
         output.WriteConstant(EtfConstants.VersionNumber);
         EtfEncoder.EncodeType(etfType, output);
+    }
+
+    public static byte[] Pack(EtfContainer etfType)
+    {
+        var mem = new byte[GetPackedSize(etfType)];
+        Pack(etfType, EtfMemory.FromArray(mem));
+        return mem;
     }
 
     public static int GetPackedSize(EtfContainer container)
